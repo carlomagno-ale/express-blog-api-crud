5ffecae1cec1e78b4842103ff3d4e97527e2d70c
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-14 15:50:53 +0100

    add controller

---
+++ routers/posts.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router();
+const postController = require('../controllers/postController');
+
+// index
+router.get('/', postController.index);
+
+//show
+router.get('/:id', postController.show);
+
+//create/store
+router.post('/', postController.store);
+
+//update
+router.put('/:id', postController.update);
+
+//update/modify 
+router.patch('/:id', /*postController.modify*/);
+
+//delete
+router.delete('/:id', postController.destroy );
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
-const port = 8000;
+const port = 3000;
+const postsRouter = require('./routers/posts')
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
+
+app.use('api/v1/posts', postsRouter)
