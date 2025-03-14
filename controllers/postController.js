97210f57f99bdf8a9b8ecb46b15f58dd79d0b715
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-14 17:11:52 +0100

    add show crud

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,15 @@
-const posts = require('../data/posts-data'); 
+const posts = require('../data/posts-data');
 
 function index(req, res) {
     res.json(posts);
 }
 function show(req, res) {
-    res.send(`visualizza solo i post con id:${req.params.id}`)
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+    // cerchiamo tramite id
+    const postSingle = posts.find(postSingle => postSingle.id === id);
+    // Restituiamolo sotto forma di JSON
+    res.json(postSingle);
 }
 function store(req, res) {
     res.send('crea un nuovo post')
@@ -21,11 +26,11 @@ function destroy(req, res) {
     res.send(`elimina un post con id ${req.params.id}`)
 }
 
-module.exports = { 
+module.exports = {
     index,
     show,
     store,
     update,
     modify,
-    destroy 
+    destroy
 }
