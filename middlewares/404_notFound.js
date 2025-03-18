e064a98ed47c213150f09989d571cfa04e489e75
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-18 16:02:19 +0100

    edit 404 middleware

--- middlewares/404_notFound.js
+++ middlewares/404_notFound.js
@@ -1,8 +1,8 @@
-function notFound (req, res, next) {
+function notFound(req, res, next) {
     res.status(404)
     res.json({
-    error: "Not Found",
-    message: "Pagina non trovata"
+        error: "Not Found",
+        message: "Pagina non trovata"
     });
-    };
-    module.exports = notFound;
+};
+module.exports = notFound;
